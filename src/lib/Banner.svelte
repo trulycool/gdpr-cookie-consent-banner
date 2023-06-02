e0d82b6d4d79e069bb85964d6efc5142022475bf
Morgan Leininger <morgan@trulycool.com>
2023-06-02 17:54:21 -0400

    Add option to hide Cookie Settings button

--- src/lib/Banner.svelte
+++ src/lib/Banner.svelte
@@ -1,152 +1,149 @@
 <script>
-  import Cookies from 'js-cookie'
-  import { validate } from './util.js'
-  import { fade } from 'svelte/transition'
-  import { onMount, createEventDispatcher } from 'svelte'
+  import Cookies from "js-cookie";
+  import { validate } from "./util.js";
+  import { fade } from "svelte/transition";
+  import { onMount, createEventDispatcher } from "svelte";
 
-  const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher();
 
-  export let cookieName = null
-  export let canRejectCookies = false
-  export let showEditIcon = true
+  export let cookieName = null;
+  export let canRejectCookies = false;
+  export let canEditCookies = true;
+  export let showEditIcon = true;
 
-  let shown = false
-  let settingsShown = false
+  let shown = false;
+  let settingsShown = false;
 
-  export let heading = 'GDPR Notice'
+  export let heading = "GDPR Notice";
   export let description =
-    'We use cookies to offer a better browsing experience, analyze site traffic, personalize content, and serve targeted advertisements. Please review our privacy policy & cookies information page. By clicking accept, you consent to our privacy policy & use of cookies.'
+    "We use cookies to offer a better browsing experience, analyze site traffic, personalize content, and serve targeted advertisements. Please review our privacy policy & cookies information page. By clicking accept, you consent to our privacy policy & use of cookies.";
 
   export let categories = {
     analytics: function () {},
     tracking: function () {},
     marketing: function () {},
-    necessary: function () {}
-  }
+    necessary: function () {},
+  };
 
-  export let cookieConfig = {}
+  export let cookieConfig = {};
 
   const defaults = {
-    sameSite: 'strict'
-  }
+    sameSite: "strict",
+  };
 
-  export let choices = {}
+  export let choices = {};
   const choicesDefaults = {
     necessary: {
-      label: 'Necessary cookies',
+      label: "Necessary cookies",
       description: "Used for cookie control. Can't be turned off.",
-      value: true
+      value: true,
     },
     tracking: {
-      label: 'Tracking cookies',
-      description: 'Used for advertising purposes.',
-      value: true
+      label: "Tracking cookies",
+      description: "Used for advertising purposes.",
+      value: true,
     },
     analytics: {
-      label: 'Analytics cookies',
+      label: "Analytics cookies",
       description:
-        'Used to control Google Analytics, a 3rd party tool offered by Google to track user behavior.',
-      value: true
+        "Used to control Google Analytics, a 3rd party tool offered by Google to track user behavior.",
+      value: true,
     },
     marketing: {
-      label: 'Marketing cookies',
-      description: 'Used for marketing data.',
-      value: true
-    }
-  }
+      label: "Marketing cookies",
+      description: "Used for marketing data.",
+      value: true,
+    },
+  };
 
-  $: choicesMerged = Object.assign({}, choicesDefaults, choices)
+  $: choicesMerged = Object.assign({}, choicesDefaults, choices);
 
   $: choicesArr = Object.values(choicesMerged).map((item, index) => {
-    return Object.assign(
-      {},
-      item,
-      { id: Object.keys(choicesMerged)[index] }
-    )
-  })
+    return Object.assign({}, item, { id: Object.keys(choicesMerged)[index] });
+  });
 
   $: cookieChoices = choicesArr.reduce((result, item) => {
-    result[item.id] = item.value ? item.value : false
-    return result
-  }, {})
+    result[item.id] = item.value ? item.value : false;
+    return result;
+  }, {});
 
   $: necessaryCookieChoices = choicesArr.reduce((result, item) => {
-    result[item.id] = item.id === 'necessary'
-    return result
-  }, {})
+    result[item.id] = item.id === "necessary";
+    return result;
+  }, {});
 
-  export let acceptLabel = 'Accept cookies'
-  export let rejectLabel = 'Reject cookies'
-  export let settingsLabel = 'Cookie settings'
-  export let closeLabel = 'Close settings'
-  export let editLabel = 'Edit cookie settings'
+  export let acceptLabel = "Accept cookies";
+  export let rejectLabel = "Reject cookies";
+  export let settingsLabel = "Cookie settings";
+  export let closeLabel = "Close settings";
+  export let editLabel = "Edit cookie settings";
 
-  export function show () {
-    shown = true
+  export function show() {
+    shown = true;
   }
 
   onMount(() => {
     if (!cookieName) {
-      throw new Error('You must set gdpr cookie name')
+      throw new Error("You must set gdpr cookie name");
     }
 
-    const cookie = Cookies.get(cookieName)
+    const cookie = Cookies.get(cookieName);
     if (!cookie) {
-      show()
+      show();
     }
 
     try {
-      const { choices } = JSON.parse(cookie)
-      const valid = validate(cookieChoices, choices)
+      const { choices } = JSON.parse(cookie);
+      const valid = validate(cookieChoices, choices);
 
       if (!valid) {
-        throw new Error('cookie consent has changed')
+        throw new Error("cookie consent has changed");
       }
 
-      execute(choices)
+      execute(choices);
     } catch (e) {
-      removeCookie()
-      show()
+      removeCookie();
+      show();
     }
-  })
+  });
 
-  function setCookie (choices) {
-    const expires = new Date()
-    expires.setDate(expires.getDate() + 365)
+  function setCookie(choices) {
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 365);
 
-    const options = Object.assign({}, defaults, cookieConfig, { expires })
-    Cookies.set(cookieName, JSON.stringify({ choices }), options)
+    const options = Object.assign({}, defaults, cookieConfig, { expires });
+    Cookies.set(cookieName, JSON.stringify({ choices }), options);
   }
 
-  function removeCookie () {
-    const { path } = cookieConfig
-    Cookies.remove(cookieName, Object.assign({}, path ? { path } : {}))
+  function removeCookie() {
+    const { path } = cookieConfig;
+    Cookies.remove(cookieName, Object.assign({}, path ? { path } : {}));
   }
 
-  function execute (chosen) {
-    const types = Object.keys(cookieChoices)
+  function execute(chosen) {
+    const types = Object.keys(cookieChoices);
 
-    types.forEach(t => {
-      const agreed = chosen[t]
+    types.forEach((t) => {
+      const agreed = chosen[t];
       if (choicesMerged[t]) {
-        choicesMerged[t].value = agreed
+        choicesMerged[t].value = agreed;
       }
       if (agreed) {
-        categories[t] && categories[t]()
-        dispatch(`${t}`)
+        categories[t] && categories[t]();
+        dispatch(`${t}`);
       }
-    })
-    shown = false
+    });
+    shown = false;
   }
 
-  function reject () {
-    setCookie(necessaryCookieChoices)
-    execute(necessaryCookieChoices)
+  function reject() {
+    setCookie(necessaryCookieChoices);
+    execute(necessaryCookieChoices);
   }
 
-  function choose () {
-    setCookie(cookieChoices)
-    execute(cookieChoices)
+  function choose() {
+    setCookie(cookieChoices);
+    execute(cookieChoices);
   }
 </script>
 
@@ -155,7 +152,8 @@
     class="cookieConsentToggle"
     aria-label={editLabel}
     on:click={show}
-    transition:fade>
+    transition:fade
+  >
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path
         d="M510.52 255.82c-69.97-.85-126.47-57.69-126.47-127.86-70.17
@@ -168,70 +166,91 @@
         368c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32
         32zm32-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33
         32-32 32zm160 128c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32
-        32-14.33 32-32 32z" />
+        32-14.33 32-32 32z"
+      />
     </svg>
   </button>
 {/if}
 
 {#if shown}
-<div class="cookieConsentWrapper" transition:fade>
-  <div class="cookieConsent">
-    <div class="cookieConsent__Left">
-      <div class="cookieConsent__Content">
-        <p class="cookieConsent__Title">{heading}</p>
-        <p class="cookieConsent__Description">
-          {@html description}
-        </p>
+  <div class="cookieConsentWrapper" transition:fade>
+    <div class="cookieConsent">
+      <div class="cookieConsent__Left">
+        <div class="cookieConsent__Content">
+          <p class="cookieConsent__Title">{heading}</p>
+          <p class="cookieConsent__Description">
+            {@html description}
+          </p>
+        </div>
+      </div>
+      <div class="cookieConsent__Right">
+        {#if canEditCookies}
+          <button
+            type="button"
+            class="cookieConsent__Button"
+            aria-label={settingsLabel}
+            on:click={() => {
+              settingsShown = true;
+            }}
+          >
+            {settingsLabel}
+          </button>
+        {/if}
+        {#if canRejectCookies}
+          <button
+            type="submit"
+            class="cookieConsent__Button"
+            on:click={reject}
+            aria-label={rejectLabel}
+          >
+            {rejectLabel}
+          </button>
+        {/if}
+        <button
+          type="submit"
+          class="cookieConsent__Button"
+          on:click={choose}
+          aria-label={acceptLabel}
+        >
+          {acceptLabel}
+        </button>
       </div>
     </div>
-    <div class="cookieConsent__Right">
-      <button
-        type="button"
-        class="cookieConsent__Button"
-        aria-label={settingsLabel}
-        on:click={() => { settingsShown = true } }>
-        {settingsLabel}
-      </button>
-      {#if canRejectCookies}
-      <button type="submit" class="cookieConsent__Button" on:click={reject} aria-label={rejectLabel}>
-        {rejectLabel}
-      </button>
-      {/if}
-      <button type="submit" class="cookieConsent__Button" on:click={choose} aria-label={acceptLabel}>
-        {acceptLabel}
-      </button>
-    </div>
   </div>
-</div>
 {/if}
 
 {#if settingsShown}
-<div class="cookieConsentOperations" transition:fade>
-  <div class="cookieConsentOperations__List">
-    {#each choicesArr as choice}
-      {#if Object.hasOwnProperty.call(choicesMerged, choice.id) && choicesMerged[choice.id]}
-        <div
-          class="cookieConsentOperations__Item"
-          class:disabled={choice.id === 'necessary'}>
-          <input
-            type="checkbox"
-            id={`gdpr-check-${choice.id}`}
-            bind:checked={choicesMerged[choice.id].value}
-            disabled={choice.id === 'necessary'} />
-          <label for={`gdpr-check-${choice.id}`}>{choice.label}</label>
-          <span class="cookieConsentOperations__ItemLabel">
-            {choice.description}
-          </span>
-        </div>
-      {/if}
-    {/each}
-    <button
-      type="submit"
-      class="cookieConsent__Button cookieConsent__Button--Close"
-      aria-label={closeLabel}
-      on:click={() => { settingsShown = false } }>
-      {closeLabel}
-    </button>
+  <div class="cookieConsentOperations" transition:fade>
+    <div class="cookieConsentOperations__List">
+      {#each choicesArr as choice}
+        {#if Object.hasOwnProperty.call(choicesMerged, choice.id) && choicesMerged[choice.id]}
+          <div
+            class="cookieConsentOperations__Item"
+            class:disabled={choice.id === "necessary"}
+          >
+            <input
+              type="checkbox"
+              id={`gdpr-check-${choice.id}`}
+              bind:checked={choicesMerged[choice.id].value}
+              disabled={choice.id === "necessary"}
+            />
+            <label for={`gdpr-check-${choice.id}`}>{choice.label}</label>
+            <span class="cookieConsentOperations__ItemLabel">
+              {choice.description}
+            </span>
+          </div>
+        {/if}
+      {/each}
+      <button
+        type="submit"
+        class="cookieConsent__Button cookieConsent__Button--Close"
+        aria-label={closeLabel}
+        on:click={() => {
+          settingsShown = false;
+        }}
+      >
+        {closeLabel}
+      </button>
+    </div>
   </div>
-</div>
 {/if}
